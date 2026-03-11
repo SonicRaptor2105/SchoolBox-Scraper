357f747d80ce2b43194f3160790c355f350f59ac
SonicRaptor2105 <sonicraptor2105@gmail.com>
2026-03-12 07:32:49 +1100

    Add files via upload

--- main.py
+++ main.py
@@ -1,23 +1,33 @@
 import requests
+from bs4 import BeautifulSoup
+import json
+import getpass
 
-loginUrl = "https://schoolbox.arden.nsw.edu.au/login/"
-
+baseURL = "https://schoolbox.arden.nsw.edu.au"
+loginURL = baseURL + "/login/"
 session = requests.Session()
 
-session.headers.update({'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'})
+def getNoti():
+    notiJSON = session.get(baseURL + "/news/lists/feed").text
+    notis = json.loads(notiJSON)
+    finalNotiList = [f"{item["title"]}: {item["blurb"]}" for item in notis]
+    return finalNotiList
+
+
 
 if __name__ == "__main__":
     u = input("Username: ")
-    p = input("Password: ")
+    p = getpass.getpass("Password: ")
 
     package = {
         "username" : u,
-        "password" : p
-    }
-
-response = session.post(loginUrl, data=package)
-
-if response.status_code == 200 and "Dashboard" in response.text:
-    print("Success! We are in.")
-else:
-    print("Login failed. Check if there was a CSRF token required.")
+        "password": p
+        }
+    
+    response = session.post(loginURL, data=package)
+    if response.url != loginURL:
+        print("Success!")
+        pageData = BeautifulSoup(response.text, "html.parser")
+        print(getNoti())
+    else:
+        print("Login failed. Please check if password is in date and credentials are correct")
